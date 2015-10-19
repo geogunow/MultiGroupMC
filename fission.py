d14a81479fa49ed71e5cc4bef11854f7b68b3160
ljeure <lukee@mit.edu>
2015-10-19 14:56:38 -0400

    Added and implemented the fission class

---
+++ fission.py
@@ -0,0 +1,31 @@
+'''
+ @file      fission.oy
+ @brief     contains the Fission class
+ @author    Luke Eure
+ @date      October 18, 2015
+'''
+
+import numpy as np
+
+class Fission():
+    
+    def __init__(self):
+        self._fission_locations = list()
+    
+    def clear(self):
+        self._fission_locations = list()
+
+    @property
+    def length(self):
+        return len(self._fission_locations)
+
+    def next(self):
+        item = self._fission_locations[0]
+        self._fission_locations.pop(0)
+        return item
+
+    def add(self, location):
+        self._fission_locations.append(location)
+
+    def location(self, num):
+        return self._fission_locations[num]
